4dbabb04e09fbf723eb6fd49fc5a171697c3c3a2
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-09 23:21:37 -0800

    fixtures and container

---
+++ src/container/Character.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { allCharacters, characterById } from '../services/arnoldApi';
+
+
+export const useCharacters = () => {
+  const [loading, loaded] = useState(true);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    allCharacters()
+      .then(characters => {
+        setCharacters(characters);
+        loaded(false);
+      });
+  }, []);
+  return {
+    loading,
+    characters
+  };
+};
+
+export const useById = id => {
+  const [loading, setLoading] = useState(true);
+  const [character, setCharacters] = useState(null);
+  useEffect(() => {
+    characterById(id)
+      .then(character => {
+        setCharacters(character);
+        setLoading(false);
+      });
+  }, [id]);
+    
+  return {
+    loading,
+    character
+  };
+};
+
+
